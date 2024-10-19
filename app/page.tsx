f33440f787fea75ebcb76a2b4bf3ee0ffc8a945a
Dhruv Kaushik <dhruvkaushik305@gmail.com>
2024-10-19 16:41:48 +0530

    added hero image and better buttons on the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,28 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="h-full flex flex-col lg:gap-10 gap-5 justify-center items-center ">
+      <Image
+        src="/heroImage.jpg"
+        alt="hero image"
+        width={400}
+        height={400}
+        style={{ filter: "blur(4px)", objectFit: "fill" }}
+        className="absolute -z-50 h-full w-full"
+      />
       <section className="flex-1 flex flex-col gap-10 justify-center items-center">
         <Image src="/logo.png" alt="logo" width={250} height={200} />
         <p className="font-rosario text-3xl lg:text-4xl text-center">
           Your <span className="text-dk-blue">classroom</span>, Your{" "}
           <span className="text-dk-blue">coding lab.</span>
         </p>
-        <Link href={"/login"}>Login</Link>
-        <Link href={"/signup"}>Signup</Link>
+        <p className="flex justify-center gap-10 items-center">
+          <Link href={"/login"} className="gradient-btn">
+            Login
+          </Link>
+          <Link href={"/signup"} className="gradient-btn">
+            Signup
+          </Link>
+        </p>
       </section>
       <footer className="text-right text-md lg:text-lg w-full">
         Coming soon...
