2d188024637d8b40ca4407829467fd2e13f35c9d
Dhruv Kaushik <dhruvkaushik305@gmail.com>
2024-10-20 16:42:46 +0530

    modified the landing page
    changed the slogan, added description and a navbar on the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -9,21 +9,29 @@ export default function Home() {
         alt="hero image"
         width={400}
         height={400}
-        style={{ filter: "blur(4px)", objectFit: "fill" }}
+        style={{ filter: "blur(6px)", objectFit: "fill" }}
         className="absolute -z-50 h-full w-full"
       />
-      <section className="flex-1 flex flex-col gap-10 justify-center items-center">
-        <Image src="/logo.png" alt="logo" width={250} height={200} />
-        <p className="font-rosario text-3xl lg:text-4xl text-center">
-          Your <span className="text-dk-blue">classroom</span>, Your{" "}
-          <span className="text-dk-blue">coding lab.</span>
+      <Navbar />
+      <section className="grow flex flex-col gap-10 justify-center items-center">
+        <p className="text-5xl font-bold lg:text-6xl text-center">
+          Streamline your{" "}
+          <span className="bg-gradient-to-t from-blue-800 to-blue-500 text-transparent bg-clip-text">
+            coding
+          </span>{" "}
+          classroom
         </p>
-        <p className="flex justify-center gap-10 items-center">
-          <Link href={"/login"} className="gradient-btn">
-            Login
-          </Link>
-          <Link href={"/signup"} className="gradient-btn">
-            Signup
+        <p className="text-2xl font-medium text-center max-w-[55%]">
+          Our coding assessment platform simplifies the process of creating,
+          administering, and grading coding assessments, saving teachers time
+          and effort.
+        </p>
+        <p>
+          <Link
+            href="/signup"
+            className="bg-gradient-to-l from-blue-800 to-blue-500 text-white py-3 px-6 rounded-lg text-xl"
+          >
+            Get Started
           </Link>
         </p>
       </section>
@@ -31,5 +39,28 @@ export default function Home() {
         Coming soon...
       </footer>
     </main>
+  );
+}
+
+function Navbar() {
+  const navButtons = [
+    { name: "Login", href: "/login" },
+    { name: "Register", href: "/signup" },
+  ];
+  return (
+    <nav className="flex justify-around items-center w-full bg-white/70 p-2">
+      <Image src="/logo.png" alt="logo" width={170} height={100} />
+      <section className="flex justify-center items-center gap-5">
+        {navButtons.map((button, index) => (
+          <Link
+            key={index}
+            href={button.href}
+            className="border-b border-white transition-colors hover:border-zinc-500 duration-300"
+          >
+            {button.name}
+          </Link>
+        ))}
+      </section>
+    </nav>
   );
 }
