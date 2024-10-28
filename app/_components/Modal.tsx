dca602b581d2eb77888c89585086db6c5c50ddb2
dhruv kaushik <dhruvkaushik305@gmail.com>
2024-10-28 10:36:56 +0530

    Changed the existing modal with state variables, removed the login/signup forms into their route folders

--- app/_components/Modal.tsx
+++ app/_components/Modal.tsx
@@ -1,19 +1,31 @@
 "use client";
+import React from "react";
 import { X } from "lucide-react";
-import { useRouter } from "next/navigation";
 
 export default function Modal({ children }: { children: React.ReactNode }) {
-  const router = useRouter();
+  const [isOpen, setIsOpened] = React.useState(false);
+
+  const closeModalHandler = () => {
+    setIsOpened(false);
+  };
+
+  const preventPropogationHandler = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+  };
+
   return (
-    <div className="fixed inset-0 bg-black/30 flex items-center justify-center z-50">
-      <div className="relative p-2 bg-white rounded-xl flex flex-col shadow-lg max-w-lg w-full">
-        <button
-          onClick={() => router.back()}
-          className="flex justify-end w-full"
-        >
+    <div
+      className="fixed inset-0 bg-black/30 flex items-center justify-center z-50"
+      onClick={closeModalHandler}
+    >
+      <div
+        className="relative p-2 bg-white rounded-xl flex flex-col shadow-lg max-w-lg w-full"
+        onClick={preventPropogationHandler}
+      >
+        <button onClick={closeModalHandler} className="flex justify-end w-full">
           <X />
         </button>
-        <section>{children}</section>
+        {isOpen && children}
       </div>
     </div>
   );
