d043e3a394ed3d5d12d40409f0cdd1feccf348f1
dhruv kaushik <dhruvkaushik305@gmail.com>
2024-11-03 22:58:04 +0530

    added deletion handler, optimized the component
    used callback and memo hooks to improve re-render performance. needs
    type fixings

--- app/dashboard/classroom/[classroomId]/test/_components/NewTest.tsx
+++ app/dashboard/classroom/[classroomId]/test/_components/NewTest.tsx
@@ -3,6 +3,7 @@
 import React, { useContext } from "react";
 import { TestType } from "@/app/types";
 import Context from "./providers";
+import { Trash } from "lucide-react";
 
 interface Props {
   id: string;
@@ -38,14 +39,19 @@ function Sidebar() {
   const { context, setContext, selectedNav, setSelectedNav } =
     useContext(Context);
 
-  const navItems: string[] = ["General"];
+  //memoising this computation
+  const navItems = React.useMemo(() => {
+    const items = ["General"]; //if this name is changed, change the conditional condition in the trash icon
 
-  //adding the questions into the navItems
-  for (let i = 0; i < context.questions.length; i++) {
-    navItems.push("Question " + (i + 1));
-  }
+    //adding the context questions into the navItems
+    for (let i = 0; i < context.questions.length; i++) {
+      items.push("Question " + (i + 1));
+    }
 
-  const addQuestionHandler = () => {
+    return items;
+  }, [context.questions]);
+
+  const addQuestionHandler = React.useCallback(() => {
     const newQuestions = [
       ...context.questions,
       { statement: "Untitled", testCases: [] },
@@ -55,23 +61,45 @@ function Sidebar() {
 
     //adding an extra 1 because state doesn't update immediately and the length will be the old length
     setSelectedNav("Question " + (context.questions.length + 1));
-  };
+  }, [context.questions, setContext, setSelectedNav]);
 
-  const navSelectionHandler = (e: React.MouseEvent<HTMLDivElement>) => {
-    setSelectedNav(e.currentTarget.innerText);
-  };
+  const navSelectionHandler = React.useCallback(
+    (e: React.MouseEvent<HTMLDivElement>) => {
+      setSelectedNav(e.currentTarget.innerText);
+    },
+    [setSelectedNav],
+  );
+
+  const deleteNavItemHandler = React.useCallback(
+    (indexOfQuestion: number) => {
+      //this index is 1-based because the 0th item is "General"
+      const newQuestions = context.questions.filter(
+        (question, index) => index !== indexOfQuestion - 1,
+      );
+
+      //the questions are re-named according to their order because of the for loop that adds questions into the navItems
+      setContext((prevContext) => ({
+        ...prevContext,
+        questions: newQuestions,
+      }));
+    },
+    [setContext, context.questions],
+  );
 
   return (
-    <nav className="h-full w-[30rem] bg-red-100 flex flex-col gap-5">
-      {navItems.map((item, index) => (
-        <div
-          key={index}
-          onClick={navSelectionHandler}
-          className={`w-full text-lg hover:font-medium cursor-pointer text-center ${item === selectedNav ? "underline" : ""}`}
-        >
-          {item}
-        </div>
-      ))}
+    <nav className="h-full w-[30rem] flex flex-col gap-5">
+      {navItems.map((item, index) => {
+        const deletetionHandler = () => deleteNavItemHandler(index);
+        return (
+          <div
+            key={index}
+            onClick={navSelectionHandler}
+            className={`w-full p-2 text-lg hover:font-medium cursor-pointer flex items-center justify-between text-center ${item === selectedNav ? "underline" : ""}`}
+          >
+            {item} {item !== "General" && <Trash onClick={deletetionHandler} />}
+          </div>
+        );
+      })}
       <button
         className="text-blue-500 hover:underline"
         onClick={addQuestionHandler}
@@ -108,5 +136,7 @@ function GeneralPage() {
 }
 
 function QuestionsPage({ index }: { index: number }) {
-  return <section>Question{index}</section>;
+  return (
+    <section className="bg-red-200 h-full w-full">Question {index}</section>
+  );
 }
